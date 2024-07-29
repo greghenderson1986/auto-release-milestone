0ea8b6bb9808db380c9310a31717bd291675e912
Greg Henderson <greg.henderson@aderant.com>
2024-07-29 08:05:35 -0400

    asdf

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@ set -u
 
 repo_token=$1
 
-if [ "$GITHuB_EVENT_NAME" != "milestone" ]; then
+if [ "$GITHUB_EVENT_NAME" != "milestone" ]; then
   echo "::debug::The event name was '$GITHUB_EVENT_NAME'"
   exit 0
 fi
-
+# hello
 event_type=$(jq --raw-output .action $GITHUB_EVENT_PATH)
 
 if [ $event_type != "closed" ]; then
